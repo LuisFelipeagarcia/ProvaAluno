a05d04cf51887c7b7eb25040d58cdbb8c89b46fe
LuisFelipeagarcia <57453710+LuisFelipeagarcia@users.noreply.github.com>
2020-10-07 15:47:29 -0300

    Projeto Finalizado

---
+++ ProvaAluno/ProvaAluno/Context/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using ProvaAluno.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ProvaAluno.Context
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<AlunoModel> Alunos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ProvaAluno/ProvaAluno/Controllers/AlunoController.cs
@@ -0,0 +1,125 @@
+﻿using ProvaAluno.Context;
+using ProvaAluno.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProvaAluno.Controllers
+{
+    public class AlunoController : Controller
+    {
+
+        private readonly Contexto db = new Contexto();
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            return View(db.Alunos.ToList());
+        }
+
+        #region Create - GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Create - POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(AlunoModel aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alunos.Add(aluno);
+                db.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+        #endregion
+
+        #region Details 
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlunoModel aluno = db.Alunos.Where(a => a.Id == id).FirstOrDefault();
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+        #endregion
+
+        #region Edit - GET
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlunoModel aluno = db.Alunos.Where(a => a.Id == id).FirstOrDefault();
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+        #endregion
+
+        #region Edit - POST
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AlunoModel aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aluno).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+        #endregion
+
+        #region Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlunoModel alunoModel = db.Alunos.Find(id);
+            if (alunoModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alunoModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AlunoModel aluno = db.Alunos.Find(id);
+            db.Alunos.Remove(aluno);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+    }
+}
